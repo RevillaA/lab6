849cae563cb583a1ae661e0e75d8e784c85d1a2f
RevillaA <revillaantonio212005@hotmail.com>
2026-01-27 11:52:40 -0500

    Proyecto base para realizar CI

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = requiere('express');
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+    res.send("Intregracion continua funcionando");
+});
+
+app.listen(port, () => {
+    console.log('Servidor esta ejecutandose en el puerto' + port);
+});
---
+++ sum.test.js
@@ -0,0 +1,5 @@
+const sum = requiere('./sum');
+
+test('Suma de 3 + 4 debe ser 7', ()=> {
+    expect(sum(3,4)).toBe(7);
+});
