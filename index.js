71b4759439d7c499f13751506157ae1cd33c750f
RevillaA <revillaantonio212005@hotmail.com>
2026-01-27 12:20:06 -0500

    Fix

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-    res.send("Intregracion continua funcionando");
+    res.send('Intregracion continua funcionando');
 });
 
 app.listen(port, () => {
