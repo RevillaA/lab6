0d3f0a9a311aa871dc9841c7b7f08f740beb840c
RevillaA <revillaantonio212005@hotmail.com>
2026-02-06 13:17:01 -0500

    Cambio intencional

--- math.test.js
+++ math.test.js
@@ -1,7 +1,7 @@
 const { factorial, fibonacci } = require('./math');
 
 test('Factorial de 5 debe ser 120', () => {
-  expect(factorial(5)).toBe(120);
+  expect(factorial(5)).toBe(100);
 });
 
 test('Fibonacci en posición 6 debe ser 8', () => {
